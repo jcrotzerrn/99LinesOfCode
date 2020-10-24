4775d6b5538760ee903dcd1a5dcbf1471838c3d0
jcrotzerrn <55601326+jcrotzerrn@users.noreply.github.com>
2020-10-23 21:04:54 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,57 +1,13 @@
-let myName = "Julie";
-
-const usStates = 52;
-
-//added 5+4
-let x = 5;
-let y = 4;
-let z = x + y;
-
-function sayHello() {
-    alert( 'Hello World!' );
-}
-
-sayHello();
-let name =''
-let age = '';
-
-function checkAge(name, age)  {
-    if (age < 21) {
- alert( "Sorry " + name + ", you aren't old enough to view this page!" )
-    } 
-}
-checkAge('John', 17);
-
-let favVeggies = ['Tomatoes', 'Potatoes', 'Corn', 'Okra', 'Green Beans'];
-
-for(let i = 0; i < favVeggies.length; i++) {
-    console.log(favVeggies[i]);
-};
-
-let pet = {name:"Fido", breed:"Yorkie"};
-
-console.log(pet);
-
-let friends = [
-    {name: 'Julia', age: 26},
-    {name: 'Kelly', age: 16},
-    {name: 'Adam', age: 30},
-    {name: 'Shelley', age: 18},
-    {name: 'Amy', age: 22}
-];
-
+let friends = ['Julia', 'Amy', 'Sarah', 'Crystal', 'Misty']
+   
 for(let i = 0; i < friends.length; i++) {
-    checkAge(friends[i].name, friends[i].age);
-}
-
-function getLength(word) {
-    return word.length;
-}
-    let howLongIsThisWord = getLength('Hello World');
-
-    if (howLongIsThisWord % 2 === 0) {
-        console.log('The world is nice and even.');
-    } else { 
-        console.log('The world is an odd place!');
+    for(let j = 99; j > 0; j--) { 
+        if(j > 0) {
+            console.log( j + " lines of code in the file, " + j + " lines of code," + friends[i] + " strikes one out, clears it all out, " + (j-1)+ " lines of code in the file"); 
+        } else {
+            console.log(j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+        break;
+        }
+        
     }
-getLength('Hello World');
+}
